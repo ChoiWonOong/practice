aac45a6ed3af338481db465840700ddd8131ed7a
ChoiWonOong <97601348+ChoiWonOong@users.noreply.github.com>
2023-05-14 23:32:15 +0900

    Update Backtrack_Knapsack.c

--- Backtrack_Knapsack.c
+++ Backtrack_Knapsack.c
@@ -6,18 +6,21 @@ struct node {
     int value;
     int weight;
 };
+int include[11] = {1,1,1,1,1,1,1,1,1,1,1};    
+int maxprofit=0;
+//item 개수 n, 최대 weight W
+int n=4; int W=16;
 
-int promising(int maxprofit, int W, int n, int* include, int* value, int* weight, struct node node);
-void knapsack(int item_index, int maxprofit, int maxweight, int n, int* include, int* value, int* weight, struct node* queue, int index);
+int promising(int* value, int* weight, struct node node);
+void knapsack(int item_index, int* value, int* weight, struct node* tree, int index);
 
 int main(){
-    int include[11] = {1,1,1,1,1,1,1,1,1,1,1};    
-    struct node queue[1025];
+    
+    struct node tree[1025];
     struct node root = { 0,0 };
-    int maxprofit=0;
-    queue[1] = root;
-    //item 개수 n, 최대 weight W
-    int n=4; int W=16;
+    
+    tree[1] = root;
+    
 
     //values
     int v[4] = { 40, 30, 50, 10 };
@@ -29,16 +32,15 @@ int main(){
     //int p = promising(W, n, include, v, w, root);
     //printf("p : %d\n", p);
    
-    knapsack(0, maxprofit, W, n, include, v, w, queue, 1);
+    knapsack(0, v, w, tree, 1);
     //printf("%d\n",maxprofit);
 
 
 }
-int promising(int maxprofit, int W, int n, int* include, int* value, int* weight, struct node node){
+int promising(int* value, int* weight, struct node node){
     int totweight = node.weight;
     float bound = (float)node.value;
-    printf("node value : %.2f\n", bound);
-    printf("node weight : %d\n", node.weight);
+
     if (node.weight >= W) return 0; 
     else{
         for(int i=0; i<n;i++){
@@ -67,43 +69,42 @@ int promising(int maxprofit, int W, int n, int* include, int* value, int* weight
         return bound > maxprofit;
     }
 }
-void knapsack(int item_index, int maxprofit, int maxweight, int n, int* include, int* value, int* weight, struct node* queue, int index){
-    if ((queue[index].weight <= maxweight) && (queue[index].value > maxprofit)) { // 지금까지 최고의 답
-		maxprofit = queue[index].value;
+void knapsack(int item_index, int* value, int* weight, struct node* tree, int index){
+    if ((tree[index].weight <= W) && (tree[index].value > maxprofit)) { // 지금까지 최고의 답
+		maxprofit = tree[index].value;
         printf("item index : %d\n",item_index);
         
 	}
-    for(int i=0; i<11; i++){
-            printf("%d ", include[i]);
-        }printf("\n");
 
-    if (promising(maxprofit, maxweight, n, include, value, weight, queue[index])) {
-        if(queue[index].value > maxweight)
-        maxweight = queue[index].value;
+    if (promising(value, weight, tree[index])) {
+        if(tree[index].value > W)
+        W = tree[index].value;
         int temp[11];
         memmove(temp, include, sizeof(int)*11);
-        
-        printf("index : %d\n", index);
+
         printf("maxprofit : %d\n\n", maxprofit);
         //make left child node
         // Include weight[i+1]
         include[item_index+1] = 1;
 
-        //connect node with queue
-        queue[index*2].value = queue[index].value+value[item_index+1];
-        queue[index*2].weight = queue[index].weight+weight[item_index+1];
+        //connect node with tree
+        tree[index*2].value = tree[index].value+value[item_index+1];
+        tree[index*2].weight = tree[index].weight+weight[item_index+1];
         //maxweight += weight[item_index];
 
-        knapsack(item_index+1, maxprofit, maxweight, n, include, value, weight, queue, index*2);
+        knapsack(item_index+1, value, weight, tree, index*2);
 
         //make right child node
         // Not include weight[i+1]
 
-        //connect node with queue
-        queue[index*2+1].value = queue[index].value;
-        queue[index*2+1].weight = queue[index].weight;
-        include = temp;
+        //connect node with tree
+        tree[index*2+1].value = tree[index].value;
+        tree[index*2+1].weight = tree[index].weight;
+
+        for(int i =0; i<11; i++){
+            include[i] = temp[i];
+        }
         include[item_index+1] = 0;
-    	knapsack(item_index+1, maxprofit, maxweight, n, include, value, weight, queue, index*2+1);
+    	knapsack(item_index+1, value, weight, tree, index*2+1);
     }
 }
